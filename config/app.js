0f4f10f6a5eafd197218edbbe584d9505a0eb6e3
adityahirparadev <166916879+adityahirparadev@users.noreply.github.com>
2024-05-16 06:45:13 +0000

    express  setup commit

---
+++ config/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+import dotenv from "dotenv";
+const  app = express();
+dotenv.config();
+
+
+
+
+export default app;
---
+++ index.js
@@ -0,0 +1,7 @@
+import app from "./config/app.js";
+
+const { PORT } = process.env;
+
+app.listen(PORT, () => {
+    console.log(`Server run at port : ${PORT}`);
+})
