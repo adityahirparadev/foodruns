a9c31de1335af712009fcf7f020f1e053aca4a25
adityahirparadev <166916879+adityahirparadev@users.noreply.github.com>
2024-05-20 03:21:40 +0000

    final way

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,47 +4,41 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg py-0 bg-light">
-        <div className="container-fluid nav_bg ">
-          <NavLink className="navbar-brand logo-skc" to="/">
-            Shree Krishna Corporation
+      <header className="header_section">
+        <nav className="main_header">
+          <NavLink className="logo" to="/">
+            Foodruns
           </NavLink>
-          <div
-            className="collapse navbar-collapse anchor_tag"
-            id="navbarSupportedContent"
-          >
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/">
-                    Home
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/user/about">
-                    About
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/user/contact">
-                    Contact
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/user/login">
-                    Login
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/user/register">
-                    SignUp
-                  </NavLink>
-                </li>
-              </>
-            </ul>
-          </div>
-        </div>
-      </nav>
+          <ul className="header_navbar">
+            <li className="">
+              <NavLink className="" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="">
+              <NavLink className="" to="/user/about">
+                About
+              </NavLink>
+            </li>
+            <li className="">
+              <NavLink className="" to="/user/contact">
+                Contact
+              </NavLink>
+            </li>
+            <li className="">
+              <NavLink className="" to="/user/login">
+                Login
+              </NavLink>
+            </li>
+            <li className="">
+              <NavLink className="" to="/user/register">
+                SignUp
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
     </>
   );
 };
